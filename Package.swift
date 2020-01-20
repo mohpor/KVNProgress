6d180cc4a0e78226071ac1fae527d34be207951a
Mohammad Porooshani <porooshani@gmail.com>
2020-01-20 13:29:57 +0330

    Added SPM Manifest
    Signed-off-by: Mohammad Porooshani <porooshani@gmail.com>

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "KVNProgress",
+    products: [
+        .library(name: "KVNProgress", targets: ["KVNProgress"])
+    ],
+    targets: [
+        .target(
+            name: "KVNProgress",
+            path: "KVNProgress",            
+            sources: ["Classes", "Categories", "Resources"]            
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
